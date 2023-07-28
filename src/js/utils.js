bcfd1ad8141221f1965b6af1bca3de28fd1ae989
Pawanyy <pawanyyy01@gmail.com>
2023-07-29 00:54:03 +0530

    commit formatted blog

--- src/js/utils.js
+++ src/js/utils.js
@@ -11,5 +11,43 @@ export function slugify(text) {
 }
 
 export function fromatDate(date) {
-return new Date(date).toLocaleDateString("en-US",{ timeZone: "UTC"});
+    return new Date(date).toLocaleDateString("en-US",{ timeZone: "UTC"});
+}
+
+export function formatBlogPosts(posts, {
+    filterOutDrafts = true,
+    filterOutFuterPosts = true,
+    sortByDate = true,
+    reverseOrder = false,
+    randomize = false,
+    limit = 0,
+} = {}) {
+
+    let filteredPosts = posts;
+    
+    if (filterOutDrafts) {
+        filteredPosts = filteredPosts.filter(post => !post.frontmatter.draft);
+    }
+    
+    if (filterOutFuterPosts) {
+        filteredPosts = filteredPosts.filter(post => new Date(post.frontmatter.date) <= new Date());
+    }
+    
+    if (sortByDate) {
+        filteredPosts = filteredPosts.sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
+    }
+    
+    if (reverseOrder) {
+        filteredPosts = filteredPosts.reverse();
+    }
+
+    if(randomize){
+        filteredPosts = filteredPosts.sort(() => Math.random() - 0.5);
+    }
+    
+    if (typeof limit === "number" && limit != 0) {
+        filteredPosts = filteredPosts.slice(0, limit);
+    }
+    
+    return filteredPosts;
 }
